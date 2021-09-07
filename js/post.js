abd1e955b4140471b47a59f97634fab6eb7e31eb
Erfan Hoseini <56768093+erfgram@users.noreply.github.com>
2021-09-07 13:14:23 +0430

    Play Gif on hover
    new feature on shot hovers

--- js/post.js
+++ js/post.js
@@ -2,6 +2,7 @@ var accessToken =
   "33ff3110d0a4e9a547c59fbd6c70d68405da66dba7c46316d3e914aae236dac2";
 url = "https://api.dribbble.com/v2/user/shots?access_token=" + accessToken;
 
+
 (function () {
   var xmlHttp;
   xmlHttp = new XMLHttpRequest();
@@ -11,39 +12,72 @@ url = "https://api.dribbble.com/v2/user/shots?access_token=" + accessToken;
       var respData = JSON.parse(xmlHttp.responseText) || {};
       console.log(respData);
       
+      function ifAnimated(shot) {
+        if (respData[shot].animated == true) {
+          return respData[shot].images.hidpi
+        } else {
+          return respData[shot].images.two_x
+        }
+      }
+
       page = document.getElementById("works");
-      var works = '<div class="rows triple"><div class="work samll"><a href="'+
-      respData[0].html_url + '" target="_blank"><img src="'+
+      var works = // triple
+      '<div class="rows triple">'+
+      // work small
+      '<div class="work small"><a href="'+
+      respData[0].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[0].images.two_x +'" alt="'+
-      respData[0].title +'"></a></div>'+
-      '<div class="work samll"><a href="'+
-      respData[1].html_url + '" target="_blank"><img src="'+
+      respData[0].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(0)+'"></a></div>'+
+      // work small
+      '<div class="work small"><a href="'+
+      respData[1].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[1].images.two_x +'" alt="'+
-      respData[1].title +'"></a></div>'+
-      '<div class="work samll"><a href="'+
-      respData[2].html_url + '" target="_blank"><img src="'+
+      respData[1].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(1)+'"></a></div>'+
+      // work small
+      '<div class="work small"><a href="'+
+      respData[2].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[2].images.two_x +'" alt="'+
-      respData[2].title +'"></a></div></div>'+
-      '<div class="rows double"><div class="work medium"><a href="'+
-      respData[3].html_url + '" target="_blank"><img src="'+
+      respData[2].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(2)+'"></a></div>'+
+      '</div>' +
+      // double
+      '<div class="rows double">'+
+      // work medium
+      '<div class="work medium"><a href="'+
+      respData[3].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[3].images.two_x +'" alt="'+
-      respData[3].title +'"></a></div>'+
-      '<div class="work medium"><a href="' +
-      respData[4].html_url + '" target="_blank"><img src="'+
+      respData[3].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(3)+'"></a></div>'+
+      // work medium
+      '<div class="work medium"><a href="'+
+      respData[4].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[4].images.two_x +'" alt="'+
-      respData[4].title +'"></a></div></div>'+
-      '<div class="rows triple"><div class="work samll"><a href="'+
-      respData[5].html_url + '" target="_blank"><img src="'+
+      respData[4].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(4)+'"></a></div>'+
+      '</div>' +
+      // triple
+      '<div class="rows triple">'+
+      // work small
+      '<div class="work small"><a href="'+
+      respData[5].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[5].images.two_x +'" alt="'+
-      respData[5].title +'"></a></div>'+
-      '<div class="work samll"><a href="'+
-      respData[6].html_url + '" target="_blank"><img src="'+
+      respData[5].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(5)+'"></a></div>'+
+      // work small
+      '<div class="work small"><a href="'+
+      respData[6].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[6].images.two_x +'" alt="'+
-      respData[6].title +'"></a></div>'+
-      '<div class="work samll"><a href="'+
-      respData[7].html_url + '" target="_blank"><img src="'+
+      respData[6].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(6)+'"></a></div>'+
+      // work small
+      '<div class="work small"><a href="'+
+      respData[7].html_url +'" target="_blank"><img class="dribbble-shot" src="'+
       respData[7].images.two_x +'" alt="'+
-      respData[7].title +'"></a></div></div>'
+      respData[7].title+'"><img class="dribbble-video" src="'+
+      ifAnimated(7)+'"></a></div>'+
+      '</div>'+'</div>'
       page.innerHTML += works;
       
     }
